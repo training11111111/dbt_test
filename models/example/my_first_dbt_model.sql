2454ccb37d9354b97ff3aa87fc3c1120faf5c50b
flhoxha33@gmail.com <flhoxha33@gmail.com>
2023-09-27 21:16:34 +0000

    inserting records

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -13,7 +13,15 @@ with source_data as (
 
     select 1 as id
     union all
-    select null as id
+    select 2 as id
+    union all
+    select 3 as id
+    union all
+    select 4 as id
+    union all
+    select 5 as id
+    union all
+    select 6 as id
 
 )
 
